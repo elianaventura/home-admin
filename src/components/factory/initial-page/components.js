7d706215ef588e8f0e7463d803b8f96139f93659
Eliana <eliana.ventura@mercadolibre.com>
2023-06-04 18:40:21 -0300

    content wip

--- src/components/factory/initial-page/components.js
+++ src/components/factory/initial-page/components.js
@@ -1,9 +1,11 @@
 import Menu from '../../menu';
 import Verticals from '../../verticals';
+import Content from '../../content';
 
 const sources = {
   'MENU': Menu,
   'VERTICALS': Verticals,
+  'CONTENT': Content,
 };
 
 export default sources;
