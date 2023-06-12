d4c0312d9205bc6a05b6fe161796e392ac3e9104
Eliana <eliana.ventura@mercadolibre.com>
2023-06-11 23:19:33 -0300

    vertical wip

---
+++ src/components/factory/vertical-icons/components.js
@@ -0,0 +1,7 @@
+import Cart from '@mui/icons-material/ShoppingCart';
+
+const sources = {
+  'SUPPLIES': Cart,
+};
+
+export default sources;
--- src/components/verticals/index.js
+++ src/components/verticals/index.js
@@ -1,7 +1,23 @@
-export default function Verticals() {
+"use client"
+
+import factoryFrom from '../factory/factory-from';
+import verticalIconComponents from '../factory/vertical-icons/components';
+
+const renderOption = function(IconComponent, { id, type, title, icon }, onSelectVertical, index) {
+  return (
+    <li className="vertical" key={index} onClick={onSelectVertical(id)} arial-aria-label={title}>
+      <IconComponent type={type} {...icon} />
+    </li>
+  ); 
+};
+
+export default function Verticals({ options }) {
+  const IconComponent = factoryFrom(verticalIconComponents);
   return (
     <div className="verticals">
-      VERTICALS
+      <ul className="verticals-list">
+        {options.map((option, index) => renderOption(IconComponent, option, () => {}, index))}
+      </ul>
     </div>
   );
 };
