41ce5aded1bac09fdccb84a262953dca2a22cf42
elianaventura <eliana.ventura@mercadolibre.com>
2023-06-12 22:06:10 -0300

    Feature/initial verticals (#6)
    * vertical wip
    
    * verticals wip
    
    * updated contract
    
    * verticals visual wip

--- src/components/verticals/index.js
+++ src/components/verticals/index.js
@@ -1,7 +1,24 @@
-export default function Verticals() {
+"use client"
+
+import factoryFrom from '../factory/factory-from';
+import verticalIconComponents from '../factory/vertical-icons/components';
+import styles from './verticals.module.scss';
+
+const renderOption = function(IconComponent, { id, type, title, icon }, onSelectVertical, index) {
   return (
-    <div className="verticals">
-      VERTICALS
+    <li className={styles.vertical} key={index} onClick={onSelectVertical(id)} aria-label={title}>
+      <IconComponent className={styles.icon} type={type} {...icon} />
+    </li>
+  ); 
+};
+
+export default function Verticals({ options }) {
+  const IconComponent = factoryFrom(verticalIconComponents);
+  return (
+    <div>
+      <ul className={styles['verticals-list']}>
+        {options.map((option, index) => renderOption(IconComponent, option, () => {}, index))}
+      </ul>
     </div>
   );
 };
