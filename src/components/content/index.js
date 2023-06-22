2fa80f091f5428dda7f5a1815e4c9a702641f4e8
Eliana <eliana.ventura@mercadolibre.com>
2023-06-22 20:24:31 -0300

    content wip

--- src/components/content/index.js
+++ src/components/content/index.js
@@ -1,7 +1,52 @@
-export default function Content() {
+"use client"
+
+import React from 'react';
+import { useState } from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import factoryFrom from '../factory/factory-from';
+import itemComponents from '../factory/items/components';
+
+function renderTab({ id, title }, index) {
   return (
-    <div className="content">
-      CONTENT
-    </div>
+    <Tab key={index} id={id} label={title} />
+  );
+};
+
+function renderItem(props, index) {
+  const ItemComponent = factoryFrom(itemComponents);
+  return <ItemComponent key={index} {...props} />
+};
+
+function renderTabContent({ id, items }, selectedTab, index) {
+  if (index !== selectedTab) {
+    return null;
+  }
+  return (
+    items && (
+      <Box key={id}>
+        <div>
+          {items.map((item, index) => renderItem(item, index))}
+        </div>
+      </Box>)
+  );
+};
+
+export default function Content({ tabs, vertical_id, selected_tab, title }) {
+  const [selectedTab, setSelectedTab] = useState(selected_tab);
+  const handleTabChange = function(event, value) {
+    setSelectedTab(value);
+  };
+
+  return (
+    <Box>
+      <Tabs value={selectedTab} onChange={handleTabChange} aria-label={title}>
+        {tabs.map((tab, index) => renderTab(tab, index))}
+      </Tabs>
+      <Box>
+        {tabs.map((tab, index) => renderTabContent(tab, selectedTab, index))}
+      </Box>
+    </Box>
   );
 };
---
+++ src/components/supplies-item/index.js
@@ -0,0 +1,9 @@
+"use client"
+
+export default function SuppliesItem() {
+  return (
+    <div>
+      ITEM DE SUPERMERCADO
+    </div>
+  );
+};
