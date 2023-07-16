34f3aaadf4379b769142ea658d5e9d8f1f4c158c
Eliana <eliana.ventura@mercadolibre.com>
2023-07-16 12:19:33 -0300

    priority wip

--- src/components/supplies-item/inputs/priority.js
+++ src/components/supplies-item/inputs/priority.js
@@ -1,6 +1,17 @@
-export default function Priority({ title }) {
+import { useState } from 'react';
+import styles from './priority-styles.module.scss';
+
+export default function Priority({ title, number }) { 
+  const [priority, setPriority] = useState(number);
+
+  const handleChange = (event) => {
+    setPriority(event.target.value);
+  };
+
   return (
     <div>
-      {title}: prioridad 2
-    </div>);
+      <span className={styles['priority-title']}>{title}</span>
+      <input className={styles['priority-input']} value={priority} onChange={handleChange} />
+    </div>
+  );
 }
