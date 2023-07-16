2df331b18ab3a6d57637fb831a9be6f12c1a1db1
Eliana <eliana.ventura@mercadolibre.com>
2023-07-16 14:02:59 -0300

    price wip

--- src/components/supplies-item/inputs/price_by_unit.js
+++ src/components/supplies-item/inputs/price_by_unit.js
@@ -1,6 +1,17 @@
-export default function PriceByUnit({ title, price }) {
+import { useState } from "react";
+
+export default function PriceByUnit({ title, price, text_by }) {
+  const [selectedPrice, setSelectedPrice] = useState(price);
+
+  const handlePriceChange = (event) => {
+    setSelectedPrice(event.target.value);
+  };
+
   return (
     <div>
-      {title}: x por unidad
+      <span>{title}</span>
+      <input value={selectedPrice.amount} onChange={handlePriceChange} />
+      <span>{text_by}</span>
+      unidad
     </div>);
 }
