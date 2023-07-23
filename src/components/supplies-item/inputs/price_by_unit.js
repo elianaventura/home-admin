ed4ba3d7ac827d9e4ea612e178e33ba0f30e74fb
Eliana <eliana.ventura@mercadolibre.com>
2023-07-23 14:28:43 -0300

    currency select wip

--- src/components/supplies-item/inputs/price_by_unit.js
+++ src/components/supplies-item/inputs/price_by_unit.js
@@ -10,9 +10,14 @@ const renderCurrency = (currency, index) => (
   <MenuItem className={styles['currency-option']} key={index} value={currency.id}>{currency.display}</MenuItem>
 );
 
-export default function PriceByUnit({ title, price, text_by }) {
+const renderUnit = (unit, index) => (
+  <MenuItem className={styles['unit-option']} key={index} value={unit.id}>{unit.display}</MenuItem>
+);
+
+export default function PriceByUnit({ title, price, unit, text_by }) {
   const [selectedPrice, setSelectedPrice] = useState(price);
   const [currency, setCurrency] = useState(price.currencies.find(curr => curr.selected))
+  const [selectedUnit, setSelectedUnit] = useState(unit.options.find(un => un.selected));
 
   const handlePriceChange = (event) => {
     setSelectedPrice(event.target.value);
@@ -25,13 +30,20 @@ export default function PriceByUnit({ title, price, text_by }) {
     }
   };
 
+  const handleUnitChange = (event) => {
+    if (event && event.target && event.target.value) {
+      const newSelectedUnit = unit.options.find(un => event.target.value === un.id);
+      setSelectedUnit(newSelectedUnit);
+    }
+  };
+
   return (
     <div>
       <span>{title}</span>
       <Select
         className={styles['currency-select']}
-        labelId="demo-simple-select-label"
-        id="demo-simple-select"
+        labelId="currency-select-label"
+        id="currency-select"
         value={currency.id}
         label={price.currency_selected_label}
         onChange={handleCurrencyChange}
@@ -40,6 +52,15 @@ export default function PriceByUnit({ title, price, text_by }) {
       </Select>
       <input className={styles['amount-input']} value={selectedPrice.amount} onChange={handlePriceChange} />
       <span>{text_by}</span>
-      unidad
+      <Select
+        className={styles['unit-select']}
+        labelId="unit-select-label"
+        id="currency-select"
+        value={selectedUnit.id}
+        label={selectedUnit.unit_select_label}
+        onChange={handleUnitChange}
+      >
+        {unit.options.map((un, index) => renderUnit(un, index))}
+      </Select>
     </div>);
 }
