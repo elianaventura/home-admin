6c08e2108cbddb02884de4877199f2de6bb8b275
Eliana <eliana.ventura@mercadolibre.com>
2023-08-12 14:25:23 -0300

    mini prueba

--- src/components/supplies-item/inputs/__tests__/consumption.test.js
+++ src/components/supplies-item/inputs/__tests__/consumption.test.js
@@ -3,7 +3,7 @@ import Consuption from "../consuption";
 
 describe('First', () => {
   it('renders passes test', () => {
-    const { container } = render(<Consuption title="Consuption" />);
+    const { container } = render(<Consuption title="Consumo" />);
  
     expect(container).toMatchSnapshot();
   });
